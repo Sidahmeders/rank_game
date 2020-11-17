61a5d8388075333fe978a0d394cc7ab2410ce24b
sidahmed <ahorob57@gmail.com>
2020-11-18 00:22:06 +0100

    getposts

--- facade/scripts.js
+++ facade/scripts.js
@@ -4,3 +4,11 @@ function getUsers() {
         headers: { "Content-Type": "application/json" }
     }).then(res => res.json())
 }
+
+function getUserPosts(userId) {
+    return fetch(`https://jsonplaceholder.typicode.com/posts/${userId}`, {
+        method: "GET",
+        headers: { "Content-Type": "application/json" }
+    }).then(res => res.json())
+}
+
