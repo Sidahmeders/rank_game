51c4371d2765a3c370b62b9eddfe1ed03dc357ed
sidahmed <ahorob57@gmail.com>
2020-11-17 21:54:06 +0100

    singlton folder

---
+++ singleton/Singleton.js
@@ -0,0 +1,14 @@
+export default class FancyLogger {
+    constructor() {
+        this.logs = []
+    }
+
+    log(message) {
+        this.logs.push(message)
+        console.log(`Fancy: ${message}`)
+    }
+
+    printLogCount() {
+        console.log(`${this.logs.length} logs`)
+    }
+}
