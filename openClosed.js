ee588ec0aa33e02c6b40b57f6222a9088d30cec0
sidahmed <ahorob57@gmail.com>
2020-11-18 16:10:20 +0100

    cool opend closed

--- openClosed.js
+++ openClosed.js
@@ -2,46 +2,47 @@
 function printQuiz(questions) {
     questions.forEach(question => {
         console.log(question.description)
-        switch (question.type) {
-            case 'boolean':
-                console.log('1, True')
-                console.log('2, False')
-                break
-            case 'multipleChoice':
-                question.options.forEach((option, index) => {
-                    console.log(`${index+1}. ${option}`)
-                })
-                break
-            case 'text':
-                console.log('Answer:____________')
-                break
-            case 'range':
-                console.log('Max:____________')
-                console.log('Min:____________')
-                break
-        }
+        question.printQuestion()
         console.log('')
     })
 }
 
-const questions = [
-    {
-        type: 'boolean',
-        description: 'this video is useful.'
-    },
-    {
-        type: 'multipleChoice',
-        description: 'What is your favorite lanuage?',
-        options: ['CSS', 'HTML', 'JS', 'Python']
-    },
-    {
-        type: 'text',
-        description: 'Descripe your favorite Js framework'
-    },
-    {
-        type: 'range',
-        description: 'what is the speed limit in your mind'
+class MultipleChoice {
+    constructor(description, options) {
+        this.description = description
+        this.options = options
     }
+
+    printQuestion() {
+        this.options.forEach((option, index) => console.log(`${index+1}) ${option}`))
+    }
+}
+
+class Boolean {
+    constructor(description) {
+        this.description = description
+    }
+
+    printQuestion() {
+        console.log('1, True')
+        console.log('2, False')
+    }
+}
+
+class Text {
+    constructor(description) {
+        this.description = description
+    }
+
+    printQuestion() {
+        console.log('Answer________')
+    }
+}
+
+const questions = [
+    new MultipleChoice('What is your favorite lanuage?', ['CSS', 'HTML', 'JS', 'Python']),
+    new Boolean('this video is useful.'),
+    new Text('Descripe your favorite Js framework')
 ]
 
 printQuiz(questions)
