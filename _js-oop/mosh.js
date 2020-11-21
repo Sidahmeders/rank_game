e9c55ae5e2f139a5c4954bfba7acbce1eba29d37
sidahmed <ahorob57@gmail.com>
2020-11-21 02:18:47 +0100

    remove this

--- _js-oop/mosh.js
+++ _js-oop/mosh.js
@@ -2,13 +2,14 @@
 function Circle(radius) {
     this.radius = radius
 
-    this.defaultLocation = {x: 0, y: 0}
+    let defaultLocation = {x: 0, y: 0}
 
-    this.computeOptimumLocation = function() {
+    let computeOptimumLocation = function(factor) {
         // do some fancy stuff
     }
 
     this.draw = function() {
+        computeOptimumLocation(3)
         console.log('drwaing..')
     }
 }
