ed4016d22763c01175f363697d0fab235f3beb23
sidahmed <ahorob57@gmail.com>
2020-11-22 10:35:40 +0100

    magazine constructor

--- _js-oop/brad.js
+++ _js-oop/brad.js
@@ -1,15 +1,15 @@
 // book constructor
-class Book {
-    constructor(name, author) {
-        this.name = name
-        this.author = author
-    }
+function Book(author, name) {
+    let prefixfornames = '-D'
+    this.name = name + prefixfornames
+    this.author = author
 
-    getYear() {
+    this.getYear = () => {
         console.log('1997')
     }
 }
 
+// instantiate book object
 const book1 = new Book('end-of-me', 'sodium')
 
 // set a method on prototypre
@@ -17,3 +17,12 @@ Book.prototype.getSummary = function() {
     return 'omin daijki'
 }
 
+// magazine constructor
+function Magazine(title, author, year, month) {
+    Book.call(this, title, author,year)
+    this.month = month
+}
+
+const magazine1 = new Magazine('cold summer', 'Jhon Doe', '2020', 'April')
+
+console.log(magazine1)
